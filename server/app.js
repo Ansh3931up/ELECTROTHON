985613f1d3a156dd109a30b7580b3decc5d3edb6
Ansh <thebeliever39@gmail.com>
2025-03-08 05:31:39 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -18,12 +18,12 @@ app.use(morgan("dev"));
 
 // Enable CORS
 const allowedOrigins = [
-  
+  process.env.CLIENT_URL,
+  process.env.FRONTEND_URL,
   'http://localhost',
   'http://localhost:3000',
   'capacitor://localhost',
   'https://localhost',
-  process.env.FRONTEND_URL
 ];
 
 app.use(
