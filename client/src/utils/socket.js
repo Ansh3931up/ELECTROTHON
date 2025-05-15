f17780ab3b29d2fac88ad29394279ba6faa873e6
Ansh <thebeliever39@gmail.com>
2025-05-15 16:51:43 +0530

    Update socket.js

--- client/src/utils/socket.js
+++ client/src/utils/socket.js
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
 // Get API base URL from environment or default to localhost
-const API_URL =  'https://13-127-217-5.nip.io';
+const API_URL =  'wss://13-127-217-5.nip.io';
 
 // Create Socket.io instance with connection options
 let socket;
